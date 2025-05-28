5f17d0d24db75b19561bc06b1de09b4be3bc6862
SamMochache <sammochache01@gmail.com>
2025-05-29 01:19:38 +0300

    Update .gitignore, settings.py, admin.py, and requirements.txt for project configuration

--- Darklight/Darklight/settings.py
+++ Darklight/Darklight/settings.py
@@ -120,10 +120,10 @@ USE_L10N = True
 USE_TZ = True
 
 # Static & Media Files
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
